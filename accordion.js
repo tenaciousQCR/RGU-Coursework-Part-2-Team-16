36b449c4fc320a83a1556977935189c7b68bff73
DReid98 <d.a.reid@rgu.ac.uk>
2016-11-08 13:42:59 +0000

    Course Overview - Accordion

---
+++ accordion.js
@@ -0,0 +1,13 @@
+/**
+ * Created by 1603232 on 08/11/2016.
+ */
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].onclick = function() {
+        this.classList.toggle("active");
+        this.nextElementSibling.classList.toggle("show");
+    }
+}
