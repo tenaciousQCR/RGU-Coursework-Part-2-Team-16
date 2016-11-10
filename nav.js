e07e2af4862dfab40662f61c94ce68eed30f26cc
DReid98 <d.a.reid@rgu.ac.uk>
2016-11-10 12:53:10 +0000

    Responsive Vertical Nav - Work In Progress

---
+++ nav.js
@@ -0,0 +1,14 @@
+/**
+ * Created by 1603232 on 10/11/2016.
+ */
+
+
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
