d4f720d962daab3347088d75b486d8f6db4fe59d
DReid98 <d.a.reid@rgu.ac.uk>
2016-11-16 10:11:23 +0000

    Course Overview Accordion

--- accordion.js
+++ accordion.js
@@ -3,11 +3,24 @@
  */
 
 var acc = document.getElementsByClassName("accordion");
+var panel = document.getElementsByClassName('panel');
 var i;
 
 for (i = 0; i < acc.length; i++) {
     acc[i].onclick = function() {
-        this.classList.toggle("active");
-        this.nextElementSibling.classList.toggle("show");
+        var setClasses = !this.classList.contains('active');
+        setClass(acc,'active','remove');
+        setClass(panel,'show','remove');
+
+        if (setClasses) {
+            this.classList.toggle("active");
+            this.nextElementSibling.classList.toggle("show");
+        }
+    }
+}
+
+function setClass(els,className,fnName) {
+    for (i = 0; i < els.length; i++) {
+        els[i].classList[fnName](className);
     }
 }
